e4e24030ddc0f6586abb69996dc3f4e6ef1a5289
xwmyth8023 <xwmyth8023@163.com>
2020-12-20 15:58:43 +0800

    point and error

---
+++ point-error/point.go
@@ -0,0 +1,53 @@
+package pointAndError
+
+import (
+	"errors"
+	"fmt"
+)
+
+var InsufficientFundsError = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Wallet struct {
+	//balance int //如果一个符号（例如变量、类型、函数等）是以小写符号开头，那么它在 定义它的包之外 就是私有的。
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+
+type Stringer interface {
+	String() string
+}
+
+//当调用一个函数或方法时，参数会被复制。
+// func (w Wallet) Deposit(amount int) {
+// 	w.balance += amount
+// }
+
+// func (w Wallet) Balance() int {
+// 	return 0
+// }
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return InsufficientFundsError
+	}
+	w.balance -= amount
+	return nil
+}
+
+func main() {
+	fmt.Printf("x")
+}
---
+++ point-error/point_test.go
@@ -0,0 +1,69 @@
+package pointAndError
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	// wallet := Wallet{}
+	// wallet.Deposit(Bitcoin(10))
+	// got := wallet.Balance()
+	// want := Bitcoin(20)
+
+	// if got != want {
+	// 	t.Errorf("got %s want %s", got, want)
+	// }
+
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	assertNoError := func (t *testing.T, got error)  {
+		if got != nil {
+			t.Fatal("got an error but didnt want one")
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want error) {
+    if got == nil {
+        t.Fatal("didn't get an error but wanted one")
+    }
+
+    if got != want {
+        t.Errorf("got '%s', want '%s'", got, want)
+    }
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		// got := wallet.Balance()
+		// want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
+		// if got != want {
+		// 	t.Errorf("got %s want %s", got, want)
+		// }
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		// got := wallet.Balance()
+		// want := Bitcoin(10)
+		assertBalance(t, wallet, Bitcoin(10))
+		// if got != want {
+		// 	t.Errorf("got %s want %s", got, want)
+		// }
+		assertNoError(t, err)
+	})
+
+	t.Run("Withdraw is over balance", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+    wallet := Wallet{startingBalance}
+    err := wallet.Withdraw(Bitcoin(100))
+
+    assertBalance(t, wallet, startingBalance)
+    assertError(t, err, InsufficientFundsError)
+	})
+}
